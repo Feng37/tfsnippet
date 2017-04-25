02e06b4b205100e510121df7af811c24ddee4671
Haowen Xu <public@korepwx.com>
2017-04-25 20:57:35 +0800

    logging drafted but not tested

---
+++ tests/scaffold/test_logging.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+
+class LoggingTestCase(unittest.TestCase):
+
+    def test_TrainLogger(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- tfsnippet/scaffold/logging.py
+++ tfsnippet/scaffold/logging.py
@@ -6,6 +6,7 @@ from collections import defaultdict, OrderedDict
 from contextlib import contextmanager
 
 import six
+import numpy as np
 import tensorflow as tf
 
 from tfsnippet.utils import (get_default_session_or_error,
@@ -22,6 +23,9 @@ VALID_TIME_METRIC = 'valid_timer'
 LOSS_METRIC = 'loss'
 TRAIN_LOSS_METRIC = 'train_loss'
 VALID_LOSS_METRIC = 'valid_loss'
+ACC_METRIC = 'acc'
+TRAIN_ACC_METRIC = 'train_acc'
+VALID_ACC_METRIC = 'valid_acc'
 
 
 class _MetricAccumulator(object):
@@ -49,6 +53,42 @@ class _MetricAccumulator(object):
     def submit(self, metric):
         self.counter += 1
         self.value += (metric - self.value) / float(self.counter)
+
+
+class _MetricBestTracker(object):
+    """Object to track the best values for certain metrics."""
+
+    def __init__(self, less_is_better=True, eps=np.finfo(float).eps):
+        self.less_is_better = less_is_better
+        self.eps = eps
+        self.value = None
+
+    @property
+    def has_value(self):
+        return self.value is not None
+
+    def get_value(self):
+        return self.value
+
+    def reset(self):
+        self.value = None
+
+    def submit(self, metric):
+        metric = float(metric)
+        if self.value is None:
+            self.value = metric
+        elif self.less_is_better:
+            self.value = min(self.value, metric)
+        else:
+            self.value = max(self.value, metric)
+
+    def is_best(self, metric):
+        if self.value is None:
+            return True
+        elif self.less_is_better:
+            return metric <= self.value + self.eps
+        else:
+            return metric >= self.value - self.eps
 
 
 def _print_function(message):
@@ -81,8 +121,19 @@ class TrainLogger(ScopedObject):
     """Loss and variables logger.
     
     This class provides convenient methods for printing training losses,
-    and for writing variable summaries onto disk.
-     
+    and for writing variable summaries onto disk.  For example:
+    
+        logger = TrainLogger(summary_dir='./summary')
+        with logger.enter_epoch():
+            for batch in minibatch_iterator(data, ignore_incomplete_batch=True):
+                with logger.enter_step():
+                    loss, _ = session.run([loss, train_op], ...)
+                    logger.add_metrics({'loss': loss})
+            with logger.enter_valid():
+                valid_loss = session.run([loss], ...)
+                logger.add_metrics({'valid_loss': valid_loss})
+            logger.print_epoch_log()
+
     Parameters
     ----------
     initial_epoch : int | tf.Tensor | tf.Variable
@@ -100,14 +151,14 @@ class TrainLogger(ScopedObject):
     metric_format : str
         String format for various metrics. (default '%.6g')
         
-    metric_name_width : int
-        Maximum width of the formatted metric name. 
+    name_width_indent : int
+        Indent width of the formatted metric names. 
         
     compact_log : bool
         Whether or not to print log messages in compact mode?
         
     print_function : (message) -> None
-        Function to print the summary messages.
+        Function to print the training logs.
         
     summary_dir : str
         Path of the TensorFlow summary directory.
@@ -116,6 +167,14 @@ class TrainLogger(ScopedObject):
     summary_log_timers : bool
         If set to True, will log epoch and step timers in TensorFlow summary.
         Default is False.
+        
+    metrics_tracking_smallest : collections.Iterable[str]
+        Name of the metrics whose smallest values should be tracked.
+        (by default ['loss', 'train_loss', 'valid_loss'] are included)
+        
+    metrics_tracking_largest : collections.Iterable[str]
+        Name of the metrics whose largest values should be tracked.
+        (by default ['acc', 'train_acc', 'valid_acc'] are included)
     
     name : str
         Name of this TrainLogger.
@@ -130,31 +189,58 @@ class TrainLogger(ScopedObject):
                  max_epoch=None,
                  max_step=None,
                  metric_format='%.6g',
-                 metric_name_width=8,
+                 name_width_indent=8,
                  compact_log=False,
                  print_function=_print_function,
                  summary_dir=None,
                  summary_log_timers=False,
+                 metrics_tracking_smallest=None,
+                 metrics_tracking_largest=None,
                  name=None,
                  default_name=None):
         # check the arguments
         initial_epoch = int(_get_tensor_value(initial_epoch))
         initial_step = int(_get_tensor_value(initial_step))
+        if max_epoch is not None:
+            max_epoch = int(max_epoch)
+        if max_step is not None:
+            max_step = int(max_step)
         if summary_dir is not None:
             summary_dir = os.path.abspath(summary_dir)
+
+        # initialize the best value tracker
+        best_tracker = {}
+        if metrics_tracking_smallest:
+            for k in metrics_tracking_smallest:
+                best_tracker[k] = _MetricBestTracker(less_is_better=True)
+        if metrics_tracking_largest:
+            for k in metrics_tracking_largest:
+                if k in self._best_tracker:
+                    raise ValueError(
+                        'Metric %r is set to track both the largest and '
+                        'smallest value, which is not supported.' % (k,)
+                    )
+                best_tracker[k] = _MetricBestTracker(less_is_better=False)
+        for k in (LOSS_METRIC, TRAIN_LOSS_METRIC, VALID_LOSS_METRIC):
+            if k not in best_tracker:
+                best_tracker[k] = _MetricBestTracker(less_is_better=True)
+        for k in (ACC_METRIC, TRAIN_ACC_METRIC, VALID_ACC_METRIC):
+            if k not in best_tracker:
+                best_tracker[k] = _MetricBestTracker(less_is_better=False)
 
         # initialize the attributes
         super(TrainLogger, self).__init__(name, default_name)
         self.epoch = initial_epoch  # type: int
         self.step = initial_step    # type: int
-        self.max_epoch = max_epoch
-        self.max_step = max_step
+        self.max_epoch = max_epoch  # type: int
+        self.max_step = max_step    # type: int
         self.metric_format = metric_format
-        self.metric_name_width = metric_name_width
+        self.name_width_indent = name_width_indent
         self.compact_log = compact_log
         self.print_function = print_function
         self.summary_dir = summary_dir
         self.summary_log_timers = summary_log_timers
+        self._best_tracker = best_tracker
 
         # the summary writer
         self._summary_writer = None
@@ -217,9 +303,33 @@ class TrainLogger(ScopedObject):
         if name in self._metrics:
             return self._metrics[name].get_value()
 
+    def is_best_metric(self, name, value):
+        """Check whether or not `value` for metric `name` is the best value.
+        
+        Parameters
+        ----------
+        name : str
+            Name of the metric.
+            
+        value : value
+            Value of the metric.
+        
+        Returns
+        -------
+        bool | None
+            True or False if `name` is configured to track the best value.
+            None if `name` is not configured.
+        """
+        best_tracker = self._best_tracker.get(name, None)
+        if best_tracker is not None:
+            return best_tracker.is_best(value)
+
     def _add_metrics(self, metrics):
         for k, v in metrics:
             self._metrics[k].submit(v)
+            best_tracker = self._best_tracker.get(k, None)
+            if best_tracker is not None:
+                best_tracker.submit(v)
 
     def add_metrics(self, metrics):
         """Add one-step metric values.
@@ -299,7 +409,7 @@ class TrainLogger(ScopedObject):
                     global_step=self.step_for_metrics
                 )
 
-    def clear_metrics(self):
+    def _clear_metrics(self):
         """Clear the accumulated metrics."""
         self._metrics.clear()
 
@@ -340,6 +450,23 @@ class TrainLogger(ScopedObject):
             if self._summary_writer:
                 self._summary_writer.flush()
 
+    def iter_epochs(self):
+        """Iterate through epochs.
+        
+        This method will yield the epoch counters.  If `max_epoch`
+        is configured, it will stop at `max_epoch` (exclusive).
+        
+        Yields
+        ------
+        int
+            The epoch counter.
+        """
+        while True:
+            with self.enter_epoch() as epoch:
+                yield epoch
+            if self.max_epoch is not None and self.epoch >= self.max_epoch:
+                break
+
     @contextmanager
     def enter_step(self):
         """Enter a training step context.
@@ -359,6 +486,29 @@ class TrainLogger(ScopedObject):
             self._step_start_time = None
             self._within_step = False
 
+    def iter_steps(self, data_iterator):
+        """Iterate through steps and epoch data batches.
+        
+        This method will yield the step counters, as well as the data
+        batches in an epoch.  It will stop at `max_step`, or when
+        `data_iterator` has been exhausted.
+        
+        Parameters
+        ----------
+        data_iterator
+            The batch data iterator.
+            
+        Yields
+        ------
+        (int, any)
+            The step counter as well as the batch data.
+        """
+        for batch in data_iterator:
+            with self.enter_step() as step:
+                yield step, batch
+            if self.max_step is not None and self.step >= self.max_step:
+                break
+
     @contextmanager
     def enter_valid(self):
         """Enter a validation context."""
@@ -376,7 +526,8 @@ class TrainLogger(ScopedObject):
             val = metric.value
             ret[key] = (
                 metric,
-                (formatter or self._format_metric_value)(val)
+                (formatter or self._format_metric_value)(val),
+                self.is_best_metric(key, val)
             )
 
         ret = OrderedDict()
@@ -384,8 +535,11 @@ class TrainLogger(ScopedObject):
                                (STEP_TIME_METRIC, _humanize_time),
                                (VALID_TIME_METRIC, _humanize_time),
                                (LOSS_METRIC, None),
+                               (ACC_METRIC, None),
                                (TRAIN_LOSS_METRIC, None),
-                               (VALID_LOSS_METRIC, None)]:
+                               (TRAIN_ACC_METRIC, None),
+                               (VALID_LOSS_METRIC, None),
+                               (VALID_ACC_METRIC, None)]:
             if key in self._metrics:
                 push_metric(key, self._metrics[key], formatter)
         for key in sorted(six.iterkeys(self._metrics)):
@@ -401,13 +555,16 @@ class TrainLogger(ScopedObject):
         else:
             ret.append('%s %d' % (cap_name, counter + 1,))
 
+        def best_mark(is_best):
+            return ' (*)' if is_best else ''
+
         metrics = self._sorted_metrics()
         if time_metric in metrics:
-            timer, formatted = metrics.pop(time_metric)
+            timer, formatted, is_best = metrics.pop(time_metric)
             if timer.counter > 1:
-                fmt = ': %ss finished in avg %%s' % name
+                fmt = ': %ss finished in avg %%s%s' % (name, best_mark(is_best))
             else:
-                fmt = ': %s finished in %%s' % name
+                fmt = ': %s finished in %%s%s' % (name, best_mark(is_best))
             ret[-1] += fmt % formatted
 
         compact_mode = self.compact_log or len(metrics) < 2
@@ -420,19 +577,22 @@ class TrainLogger(ScopedObject):
 
             if compact_mode:
                 ret.extend((
-                    '%s: %s' % (metric_name(m, k), s)
-                    for k, (m, s) in six.iteritems(metrics)
+                    '%s: %s%s' % (metric_name(m, k), s, best_mark(b))
+                    for k, (m, s, b) in six.iteritems(metrics)
                 ))
             else:
-                metric_indent = max(len(k) for k in six.iterkeys(metrics))
-                metric_indent = (
-                    ((metric_indent + 2) + self.metric_name_width - 1) //
-                    self.metric_name_width * self.metric_name_width
+                metric_indent = max(
+                    len(metric_name(m, k))
+                    for k, (m, _, _) in six.iteritems(metrics)
                 )
-                metric_fmt = '%' + str(metric_indent) + 's: %s'
+                metric_indent = (
+                    (4 + metric_indent + self.name_width_indent - 1) //
+                    self.name_width_indent * self.name_width_indent
+                )
+                metric_fmt = '%' + str(metric_indent) + 's: %s%s'
                 ret.extend((
-                    metric_fmt % (metric_name(m, k), s)
-                    for k, (m, s) in six.iteritems(metrics)
+                    metric_fmt % (metric_name(m, k), s, best_mark(b))
+                    for k, (m, s, b) in six.iteritems(metrics)
                 ))
         return ('; ' if compact_mode else '\n').join(ret)
 
@@ -440,10 +600,13 @@ class TrainLogger(ScopedObject):
         """Get epoch(s) log messages.
         
         This method will commit and clear the timer of current epoch.
+        It will also clear all the metrics afterwards.
         """
         self._commit_epoch_time()
-        return self._get_messages(
+        ret = self._get_messages(
             EPOCH_TIME_METRIC, 'epoch', self.epoch_for_metrics, self.max_epoch)
+        self._clear_metrics()
+        return ret
 
     def print_epoch_log(self):
         """Print epoch(s) log messages.
@@ -452,16 +615,18 @@ class TrainLogger(ScopedObject):
         It will also clear all the metrics afterwards.
         """
         self.print_function(self.get_epoch_log())
-        self.clear_metrics()
 
     def get_step_log(self):
         """Get step(s) log messages.
         
         This method will commit and clear the timer of current step.
+        It will also clear all the metrics afterwards.
         """
         self._commit_step_time()
-        return self._get_messages(
+        ret = self._get_messages(
             STEP_TIME_METRIC, 'step', self.step_for_metrics, self.max_step)
+        self._clear_metrics()
+        return ret
 
     def print_step_log(self):
         """Print step(s) log messages.
@@ -470,4 +635,3 @@ class TrainLogger(ScopedObject):
         It will also clear all the metrics afterwards.
         """
         self.print_function(self.get_step_log())
-        self.clear_metrics()
