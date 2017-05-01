6fd4e1355d02b0280e6ff7bae358b47c1d6f70ad
Haowen Xu <public@korepwx.com>
2017-05-01 21:11:44 +0800

    added

---
+++ tests/bayes/test_utils.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+import numpy as np
+import tensorflow as tf
+
+from tfsnippet import bayes
+from tests.helper import TestCase
+
+
+class UtilsTestCase(TestCase):
+
+    def test_local_log_prob(self):
+        with tf.Graph().as_default(), tf.Session().as_default() as sess:
+            a = bayes.Normal(0., [1., 2., 3.], sample_num=16,
+                             group_event_ndims=1)
+            b = bayes.Normal(1., 2., sample_num=16)
+            a_prob, b_prob = bayes.local_log_prob([a, b])
+
+            self.assertIsInstance(a_prob, tf.Tensor)
+            self.assertEqual(a_prob.get_shape().as_list(), [16])
+            res, ans = sess.run([
+                a_prob,
+                a.distribution.log_prob(a, group_event_ndims=1)
+            ])
+            np.testing.assert_almost_equal(res, ans)
+
+            self.assertIsInstance(b_prob, tf.Tensor)
+            self.assertEqual(b_prob.get_shape().as_list(), [16])
+            res, ans = sess.run([
+                b_prob,
+                b.distribution.log_prob(b, group_event_ndims=0)
+            ])
+            np.testing.assert_almost_equal(res, ans)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tfsnippet/bayes/utils.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+import tensorflow as tf
+
+from .layers import StochasticTensor
+
+__all__ = ['local_log_prob']
+
+
+def local_log_prob(tensors, name=None):
+    """Compute the conditional log-prob of each stochastic tensor.
+    
+    This methods computes the conditional log-probability of each specified
+    stochastic tensor, given all its ancestors.  It is like a local detector,
+    where only the tensor itself as well as its inputs are available for
+    computing the log-prob of each stochastic tensor, thus it is named as
+    `local_log_prob`.
+    
+    Parameters
+    ----------
+    tensors : collections.Iterable[StochasticTensor]
+        The stochastic tensors whose log-prob should be computed.
+        
+    name : str
+        Optional name of this operation.
+        
+    Returns
+    -------
+    tuple[tf.Tensor]
+        The computed log-prob as tensors. 
+    """
+    tensors = tuple(tensors)
+    if not tensors:
+        return ()
+    for t in tensors:
+        if not isinstance(t, StochasticTensor):
+            raise TypeError('%r is not a StochasticTensor.' % (t,))
+    with tf.name_scope(name, default_name='local_log_prob', values=tensors):
+        return tuple(t.log_prob() for t in tensors)
